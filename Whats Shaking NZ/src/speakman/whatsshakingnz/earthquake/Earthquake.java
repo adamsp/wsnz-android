00ed83c516979be529dd543026ae85e58168b9f4
Adam Speakman <adam@speakman.net.nz>
2012-10-28 15:33:33 +1300

    Added unit tests for Earthquake class

--- Whats Shaking NZ/src/speakman/whatsshakingnz/earthquake/Earthquake.java
+++ Whats Shaking NZ/src/speakman/whatsshakingnz/earthquake/Earthquake.java
@@ -149,4 +149,77 @@ public class Earthquake extends OverlayItem implements Parcelable {
 		dest.writeString(mAgency);
 		dest.writeString(mStatus);
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((mAgency == null) ? 0 : mAgency.hashCode());
+		result = prime * result + ((mDate == null) ? 0 : mDate.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(mDepth);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result
+				+ ((mLocation == null) ? 0 : mLocation.hashCode());
+		temp = Double.doubleToLongBits(mMagnitude);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((mPoint == null) ? 0 : mPoint.hashCode());
+		result = prime * result
+				+ ((mReference == null) ? 0 : mReference.hashCode());
+		temp = Double.doubleToLongBits(mRoundedMagnitude);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((mStatus == null) ? 0 : mStatus.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Earthquake other = (Earthquake) obj;
+		if (mAgency == null) {
+			if (other.mAgency != null)
+				return false;
+		} else if (!mAgency.equals(other.mAgency))
+			return false;
+		if (mDate == null) {
+			if (other.mDate != null)
+				return false;
+		} else if (!mDate.equals(other.mDate))
+			return false;
+		if (Double.doubleToLongBits(mDepth) != Double
+				.doubleToLongBits(other.mDepth))
+			return false;
+		if (mLocation == null) {
+			if (other.mLocation != null)
+				return false;
+		} else if (!mLocation.equals(other.mLocation))
+			return false;
+		if (Double.doubleToLongBits(mMagnitude) != Double
+				.doubleToLongBits(other.mMagnitude))
+			return false;
+		if (mPoint == null) {
+			if (other.mPoint != null)
+				return false;
+		} else if (!mPoint.equals(other.mPoint))
+			return false;
+		if (mReference == null) {
+			if (other.mReference != null)
+				return false;
+		} else if (!mReference.equals(other.mReference))
+			return false;
+		if (Double.doubleToLongBits(mRoundedMagnitude) != Double
+				.doubleToLongBits(other.mRoundedMagnitude))
+			return false;
+		if (mStatus == null) {
+			if (other.mStatus != null)
+				return false;
+		} else if (!mStatus.equals(other.mStatus))
+			return false;
+		return true;
+	}
 }
