229761e460dbe04d4110fbd7db287faaff7c96da
Adam Speakman <adam@speakman.net.nz>
2013-08-03 13:21:09 +1200

    Markers now display a clickable info window

--- Whats Shaking NZ/src/speakman/whatsshakingnz/fragments/NZMapFragment.java
+++ Whats Shaking NZ/src/speakman/whatsshakingnz/fragments/NZMapFragment.java
@@ -1,24 +1,31 @@
 package speakman.whatsshakingnz.fragments;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Display;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.TextView;
 import com.google.android.gms.maps.*;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.CameraPosition;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.*;
 import com.google.android.maps.GeoPoint;
+import speakman.whatsshakingnz.R;
 import speakman.whatsshakingnz.activities.QuakeActivity;
 import speakman.whatsshakingnz.earthquake.Earthquake;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
-public class NZMapFragment extends SupportMapFragment {
+public class NZMapFragment extends SupportMapFragment implements GoogleMap.InfoWindowAdapter, GoogleMap.OnInfoWindowClickListener {
     private ArrayList<Earthquake> mQuakes;
     private static final double NZ_CENTRE_LATITUDE = -41;
     private static final double NZ_CENTRE_LONGITUDE = 173;
+    private HashMap<String, Earthquake> mMarkerIdToQuake;
+
+    public NZMapFragment() {
+        mMarkerIdToQuake = new HashMap<String, Earthquake>();
+    }
 
     public static NZMapFragment newInstance() {
         GoogleMapOptions defaultOptions = new GoogleMapOptions();
@@ -51,25 +58,33 @@ public class NZMapFragment extends SupportMapFragment {
         GoogleMap map = getMap();
         if (map == null || null == mQuakes)
             return;
+
+        map.setOnInfoWindowClickListener(this);
+
         // Move the map view if this is a QuakeActivity, will
         // only have one quake in this instance.
         if (getActivity() instanceof QuakeActivity) {
-            LatLng latlng = getLatLngForQuake(mQuakes.get(0));
+            Earthquake singleQuake = mQuakes.get(0);
+            LatLng latlng = getLatLngForQuake(singleQuake);
             CameraUpdate update = CameraUpdateFactory.newLatLngZoom(latlng,
                     getDefaultZoomForDevice() + 2);
             map.moveCamera(update);
-        }
-        for (Earthquake q : mQuakes) {
-            MarkerOptions marker = getMarkerForQuake(q);
-            map.addMarker(marker);
+            map.addMarker(getMarkerForQuake(singleQuake));
+        } else {
+            for (Earthquake q : mQuakes) {
+                MarkerOptions markerOptions = getMarkerForQuake(q);
+                // Specify a title so we get an info window when tapped.
+                markerOptions.title(q.getFormattedMagnitude())
+                        .snippet(q.getFormattedDepth());
+                Marker marker = map.addMarker(markerOptions);
+                mMarkerIdToQuake.put(marker.getId(), q);
+            }
         }
     }
 
     private MarkerOptions getMarkerForQuake(Earthquake q) {
         MarkerOptions m = new MarkerOptions()
                 .position(getLatLngForQuake(q))
-                .title(q.getFormattedMagnitude())
-                .snippet(q.getFormattedDepth())
                 .icon(BitmapDescriptorFactory.defaultMarker(getHueForQuake(q)));
         return m;
     }
@@ -107,15 +122,48 @@ public class NZMapFragment extends SupportMapFragment {
      * device.
      */
     private int getDefaultZoomForDevice() {
-        WindowManager wm = (WindowManager) getActivity().getApplicationContext().getSystemService(Context.WINDOW_SERVICE);
-        Display display = wm.getDefaultDisplay();
-        int width = display.getWidth();
+        int defaultZoom = 5;
+        View v = getView();
+        if (v != null) {
+            int width = v.getWidth();
 
-        if (width < 480)
-            return 5;
-        else if (width < 720)
-            return 6;
-        else
-            return 7;
+            if (width < 480)
+                defaultZoom = 5;
+            else if (width < 720)
+                defaultZoom = 6;
+            else
+                defaultZoom = 7;
+        }
+        return defaultZoom;
+    }
+
+    @Override
+    public View getInfoWindow(Marker marker) {
+        View v = getLayoutInflater(null).inflate(R.layout.quake_marker_view, null);
+        final Earthquake quake = mMarkerIdToQuake.get(marker.getId());
+        if(quake != null) {
+            // TODO Improve this info window layout etc.
+            TextView tv = (TextView)v.findViewById(R.id.quake_marker_magnitude);
+            tv.setText(quake.getFormattedMagnitude());
+
+            tv = (TextView)v.findViewById(R.id.quake_marker_depth);
+            tv.setText(quake.getFormattedDepth());
+        }
+        return v;
+    }
+
+    @Override
+    public View getInfoContents(Marker marker) {
+        return null;
+    }
+
+    @Override
+    public void onInfoWindowClick(Marker marker) {
+        Earthquake quake = mMarkerIdToQuake.get(marker.getId());
+        if(quake != null) {
+            Intent intent = new Intent(getActivity(), QuakeActivity.class);
+            intent.putExtra(QuakeActivity.QUAKE_KEY, quake);
+            getActivity().startActivity(intent);
+        }
     }
 }
