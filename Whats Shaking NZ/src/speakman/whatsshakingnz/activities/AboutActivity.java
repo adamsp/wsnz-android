d5818d88935afef5bec2b8264eeccf4b72d56485
Adam Speakman <adam@speakman.net.nz>
2013-12-27 13:30:15 +1300

    Added licenses page.

--- Whats Shaking NZ/src/speakman/whatsshakingnz/activities/AboutActivity.java
+++ Whats Shaking NZ/src/speakman/whatsshakingnz/activities/AboutActivity.java
@@ -1,7 +1,14 @@
 package speakman.whatsshakingnz.activities;
 
 import speakman.whatsshakingnz.R;
+import speakman.whatsshakingnz.fragments.LicensesFragment;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
 
 public class AboutActivity extends WhatsShakingActivity {
 
@@ -9,6 +16,38 @@ public class AboutActivity extends WhatsShakingActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getSupportMenuInflater().inflate(R.menu.activity_about, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case R.id.menu_licenses:
+            onLicensesClick();
+            break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onLicensesClick() {
+        // Create & show a licenses fragment just as you would any other
+        // DialogFragment.
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        Fragment prev = getSupportFragmentManager().findFragmentByTag(
+                "licensesDialogFragment");
+        if (prev != null) {
+            ft.remove(prev);
+        }
+        ft.addToBackStack(null);
+
+        // Create and show the dialog.
+        DialogFragment newFragment = LicensesFragment.newInstance();
+        newFragment.show(ft, "licensesDialogFragment");
     }
 
 }
