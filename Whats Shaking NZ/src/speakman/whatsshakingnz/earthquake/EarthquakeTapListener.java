fb7aa604942a6960e9a154eea00dc43fe83c553f
Adam Speakman <adam.speakman@gmail.com>
2015-05-31 14:45:00 -0400

    Let's start again, shall we?

--- Whats Shaking NZ/src/speakman/whatsshakingnz/earthquake/EarthquakeTapListener.java
+++
@@ -1,9 +0,0 @@
-package speakman.whatsshakingnz.earthquake;
-
-/**
- * Created by Adam on 10/08/13.
- */
-public interface EarthquakeTapListener {
-    public void onEarthquakeTap(Earthquake quake);
-    public void onEarthquakeLostFocus(Earthquake quake);
-}
