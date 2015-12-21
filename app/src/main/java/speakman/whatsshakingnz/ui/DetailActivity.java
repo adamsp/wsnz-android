1cfa6683a987c7a397e264c458ebbde93bc1bc1e
Adam Speakman <adam.speakman@gmail.com>
2015-12-20 23:08:47 -0500

    Specify simple activity transition from list to detail page

--- app/src/main/java/speakman/whatsshakingnz/ui/DetailActivity.java
+++ app/src/main/java/speakman/whatsshakingnz/ui/DetailActivity.java
@@ -20,13 +20,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.TextView;
 
 import com.google.android.gms.maps.MapView;
 
--- app/src/main/java/speakman/whatsshakingnz/ui/EarthquakeListAdapter.java
+++ app/src/main/java/speakman/whatsshakingnz/ui/EarthquakeListAdapter.java
@@ -16,7 +16,12 @@
 
 package speakman.whatsshakingnz.ui;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
+import android.os.Build;
+import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -59,6 +64,17 @@ public class EarthquakeListAdapter extends RecyclerView.Adapter<EarthquakeListVi
 
     @Override
     public void onClick(View v, Earthquake earthquake) {
-        v.getContext().startActivity(DetailActivity.createIntent(v.getContext(), earthquake));
+        Context ctx = v.getContext();
+        ActivityOptionsCompat options = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP && ctx instanceof Activity) {
+            options = ActivityOptionsCompat.
+                    makeSceneTransitionAnimation((Activity) ctx, v, v.getTransitionName());
+        }
+        Intent intent = DetailActivity.createIntent(ctx, earthquake);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN && options != null) {
+            ctx.startActivity(intent, options.toBundle());
+        } else {
+            ctx.startActivity(intent);
+        }
     }
 }
