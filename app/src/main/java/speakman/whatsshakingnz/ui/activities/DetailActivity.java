8082784df575214ced8b6f90e2c205a867e092f7
Adam Speakman <adam.speakman@gmail.com>
2016-03-24 17:27:02 -0400

    Remove status bars from detail, map activities
    Moved to a base theme with primary and accent colours etc, with custom
    status bar styles for different API levels. Fixed a bunch of warnings
    in the process.

--- app/src/main/java/speakman/whatsshakingnz/ui/activities/DetailActivity.java
+++ app/src/main/java/speakman/whatsshakingnz/ui/activities/DetailActivity.java
@@ -21,7 +21,7 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
@@ -67,13 +67,14 @@ public class DetailActivity extends AppCompatActivity implements OnMapReadyCallb
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
         realm = Realm.getDefaultInstance();
         setContentView(R.layout.activity_detail);
         expandableDetailCard = (ExpandableDetailCard) findViewById(R.id.activity_map_detail_card);
         mapView = (MapView) findViewById(R.id.activity_detail_map);
         mapView.onCreate(savedInstanceState == null ? null : savedInstanceState.getBundle("mapState"));
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
         if (savedInstanceState == null && getIntent().getBooleanExtra(EXTRA_FROM_NOTIFICATION, false)) {
             logNotificationClick();
         }
--- app/src/main/java/speakman/whatsshakingnz/ui/activities/MapActivity.java
+++ app/src/main/java/speakman/whatsshakingnz/ui/activities/MapActivity.java
@@ -19,10 +19,10 @@ package speakman.whatsshakingnz.ui.activities;
 import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.animation.AccelerateInterpolator;
 import android.view.animation.DecelerateInterpolator;
@@ -69,8 +69,9 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_map);
-        setSupportActionBar((Toolbar) findViewById(R.id.activity_map_toolbar));
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
         setContentView(R.layout.activity_map);
         realm = Realm.getDefaultInstance();
         detailView = (ExpandableDetailCard) findViewById(R.id.activity_map_detail_card);
